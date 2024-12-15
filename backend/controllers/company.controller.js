991e1c6290806ac210896dd76050ff0acfa6e848
Code2With-Pratik <pratikdhandare1204@gmail.com>
2024-12-15 08:55:33 +0530

    get company by their id section done

--- backend/controllers/company.controller.js
+++ backend/controllers/company.controller.js
@@ -40,6 +40,10 @@ export const getCompany = async (req, res) => {
                 success: false
             })
         }
+        return res.status(200).json({
+            companies,
+            success:true
+        })
     } catch (error) {
         console.log(error);
     }
@@ -47,7 +51,18 @@ export const getCompany = async (req, res) => {
 // get company by id
 export const getCompanyById = async (req, res) => {
     try {
-        
+        const companyId = req.params.id;
+        const company = await Company.findById(companyId);
+        if (!company) {
+            return res.status(404).json({
+                message: "Company not found.",
+                success: false
+            })
+        }
+        return res.status(200).json({
+            company,
+            success: true
+        })
     } catch (error) {
         console.log(error);
     }
