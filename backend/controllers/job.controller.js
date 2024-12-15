8ae613f18d1e20a0c5b556147583f8095d3cc177
Code2With-Pratik <pratikdhandare1204@gmail.com>
2024-12-15 10:35:52 +0530

    post job section created

--- backend/controllers/job.controller.js
+++ backend/controllers/job.controller.js
@@ -58,3 +58,38 @@ export const getAllJobs = async (req, res) => {
         console.log(error);
     }
 }
+// student
+export const getJobById = async (req, res) => {
+    try {
+        const jobId = req.params.id;
+        const job = await Job.findById(jobId);
+        if (!job) {
+            return res.status(404).json({
+                message: "Jobs not found.",
+                success: false
+            })
+        };
+        return res.status(200).json({ job, success: true });
+    } catch (error) {
+        console.log(error);
+    }
+}
+// admin kitne job create kra hai abhi tk
+export const getAdminJobs = async (req, res) => {
+    try {
+        const adminId = req.id;
+        const jobs = await Job.find({ created_by: adminId });
+        if (!jobs) {
+            return res.status(404).json({
+                message: "Jobs not found.",
+                success: false
+            })
+        };
+        return res.status(200).json({
+            jobs,
+            success: true
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
