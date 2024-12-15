5017a24bcd8640c56dff17746dddb635da81ad52
Code2With-Pratik <pratikdhandare1204@gmail.com>
2024-12-15 09:53:07 +0530

    get company routes cteated

--- backend/controllers/company.controller.js
+++ backend/controllers/company.controller.js
@@ -69,7 +69,24 @@ export const getCompanyById = async (req, res) => {
 }
 export const updateCompany = async (req, res) => {
     try {
-        
+        const { name, description, website, location } = req.body;
+        const file = req.file;
+         // idhar cloudinary ayega
+
+        const updateData = { name, description, website, location};
+
+        const company = await Company.findByIdAndUpdate(req.params.id, updateData, { new: true });
+
+        if (!company) {
+            return res.status(404).json({
+                message: "Company not found.",
+                success: false
+            })
+        }
+        return res.status(200).json({
+            message:"Company information updated.",
+            success:true
+        })
     } catch (error) {
         console.log(error);
     }
