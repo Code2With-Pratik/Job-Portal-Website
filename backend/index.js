2afd4c6cecf7749ef4006a3916afaf1b359a6c4b
Code2With-Pratik <pratikdhandare1204@gmail.com>
2024-12-15 12:26:15 +0530

    application routers created

--- backend/index.js
+++ backend/index.js
@@ -30,6 +30,8 @@ const PORT = process.env.PORT || 3000;
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
+app.use("/api/v1/application", applicationRoute);
+
 
 
 app.listen(PORT,()=>{
